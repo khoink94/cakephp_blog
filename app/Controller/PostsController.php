e90b90f45f83f56ddfdb7974688a5f0f645dda50
khoink94 <khoink94@gmail.com>
2015-10-16 13:09:00 +0900

    first commit

---
+++ app/Controller/PostsController.php
@@ -0,0 +1,49 @@
+<?php
+class PostsController extends AppController {
+	public $helpers = array('Html', 'Form');
+
+	public function index() {
+		$this->set('posts', $this->Post->find('all'));
+	}
+	public function view($id = null) {
+		if (!$id) {
+			throw new NotFoundException(__('Invalid post'));
+		}
+		$post = $this->Post->findById($id);
+		if (!$post) {
+			throw new NotFoundException(__('Invalid post'));
+		}
+		$this->set('post', $post);
+	}
+	public function add() {
+		if ($this->request->is('post')) {
+			$this->Post->create();
+			if ($this->Post->save($this->request->data)) {
+			$this->Flash->success(__('Your post has been saved.'));
+			return $this->redirect(array('action' => 'index'));
+			}
+		$this->Flash->error(__('Unable to add your post.'));
+		}
+	}
+	public function edit($id = null) {
+		if (!$id) {
+			throw new NotFoundException(__('Invalid post'));
+		}
+		$post = $this->Post->findById($id);
+		if (!$post) {
+			throw new NotFoundException(__('Invalid post'));
+		}
+		if ($this->request->is(array('post', 'put'))) {
+			$this->Post->id = $id;
+			if ($this->Post->save($this->request->data)) {
+				$this->Flash->success(__('Your post has been updated.'));
+				return $this->redirect(array('action' => 'index'));
+			}
+			$this->Flash->error(__('Unable to update your post.'));
+			}
+		if (!$this->request->data) {
+			$this->request->data = $post;
+		}
+	}
+}
+?>
---
+++ app/Model/Post.php
@@ -0,0 +1,12 @@
+<?php
+class Post extends AppModel {
+	public $validate = array(
+		'title' => array(
+			'rule' => 'notBlank'
+		),
+		'body' => array(
+			'rule' => 'notBlank'
+		)
+	);
+}
+?>
